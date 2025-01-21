3a7e121bd8068ac023746809ef9d3e6d5abd8efe
kimuhooo <fantianmucun@gmail.com>
2025-01-21 23:19:44 +0900

    first

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,19 +1,17 @@
-let offset = -200;
-
 window.addEventListener("DOMContentLoaded", () => {
     let strHtml = `\n`;
     strHtml += `\t<img src="static/image/humbergerIcon.svg" id="menuIcon" class="menuIcon"/>\n`;
-    strHtml += `\t<div id="sideBar">\n`;
+    strHtml += `\t<div id="sideBar" class="open">\n`; // デフォルトで開いた状態にする
     strHtml += `\t\t<img src="static/image/humbergerIcon.svg" id="menuIcon2" class="menuIcon"/>\n`;
     strHtml += `\t\t<div id="menuList">\n`;
     strHtml += `\t\t\t<div id="home" class="menuItem">ホーム</div>\n`;
     strHtml += `\t\t\t<div id="reservation" class="menuItem">予約</div>\n`;
     strHtml += `\t\t\t<div id="simulate" class="menuItem">シミュレート</div>\n`;
     strHtml += `\t\t\t<div id="mypage" class="menuItem">マイページ</div>\n`;
-    strHtml += `\t\t\t<a href="https://docs.google.com/forms/d/1I9mZq3OCbmeKt92phxJntnn2AkP_-rNg97GV5uBIxYE/prefill" id="business" class="menuItem">事業者</a>\n`; // Googleフォームへのリンク
+    // strHtml += `\t\t\t<a href="https://docs.google.com/forms/d/1I9mZq3OCbmeKt92phxJntnn2AkP_-rNg97GV5uBIxYE/prefill" id="business" class="menuItem">事業者</a>\n`; // Googleフォームへのリンク
     strHtml += `\t\t</div>\n`;
     strHtml += `\t</div>\n`;
-    strHtml += `\t<div id="cover" class="cover"></div>\n`;
+    strHtml += `\t<div id="cover" class="cover coverVisible"></div>\n`; // デフォルトでカバーも表示
     menuSideBar.insertAdjacentHTML("beforeend", strHtml);
 
     // メニューアイコンのクリックイベント
